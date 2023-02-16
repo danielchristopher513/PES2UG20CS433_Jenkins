9a758987501807d50bb3129ddc1d0b842fad76ed
J P DANIEL CHRISTOPHER <danielchristopher513@gmail.com>
2023-02-16 09:29:35 +0530

    Update working.cpp

--- working.cpp
+++ working.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout < " Jenkins Pipeline\n";
+  cout << " Jenkins Pipeline\n";
   cout<<"Jenkins Pipeline executing\n" ;
   cout << "I have successfully built and run\n";
   return 0;
