5c9ade3603add40786e4db9a2e01a44f9844782e
J P DANIEL CHRISTOPHER <danielchristopher513@gmail.com>
2023-02-16 08:39:14 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"After adding web hooks and running \n";
+  cout<<"After adding web hooks and run \n";
   return 0;
 }
